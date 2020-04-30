4685517a27b63d909654d598591644d12313cfd2
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-30 14:18:22 +0900

    cleaned up boilerplate files and added hello component

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Hello from './components/hello';
+
+function App() {
+  return <Hello />
+}
+
+export default App;
---
+++ src/components/hello.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  render() {
+    return "working"
+  }
+}
+
+export default Hello;
