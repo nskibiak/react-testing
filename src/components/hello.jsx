de969209e640262139b2f40d2cc0fb528a1cf6b1
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-30 14:25:36 +0900

    added prop-types and added requirement to Hello component for prop.name

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Hello extends Component {
   render() {
-    return "working"
+    return (
+      <div>Hello {this.props.name}, how are you?</div>
+    );
   }
+}
+
+Hello.propTypes = {
+ name: PropTypes.string.isRequired
 }
 
 export default Hello;
